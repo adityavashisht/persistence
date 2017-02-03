ba610a908809340a7e9253f82546292f08ffd351
Ady <aditya@indasil.com>
2017-02-03 16:33:44 -0500

    Spring/Hibernate - Simple example

--- src/test/java/com/springexample/persistence/service/PersistenceServiceTest.java
+++ src/test/java/com/springexample/persistence/service/PersistenceServiceTest.java
@@ -1,11 +1,13 @@
 package com.springexample.persistence.service;
 
+import com.springexample.persistence.domain.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.util.Assert;
+import org.springframework.transaction.annotation.Transactional;
 
 
 /**
@@ -13,6 +15,7 @@ import org.springframework.util.Assert;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"/persistence.xml"})
+@Transactional
 public class PersistenceServiceTest {
 
     @Autowired
@@ -20,8 +23,13 @@ public class PersistenceServiceTest {
 
 
     @Test
+    @Rollback(value = false)
     public void testService() {
-        Assert.notNull(personService);
+        Person person = new Person();
+        person.setFirstName("Aditya");
+        person.setLastName("VAshisht");
+
+        personService.save(person);
     }
 
 
